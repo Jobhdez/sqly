3c50765ef04ebe36c14f880f1fdfaa3c03ed5a82
Job Hernandez <jobhdezlara93@gmail.com>
2024-10-01 11:33:46 -0700

    add support for AND

--- sql.rkt
+++ sql.rkt
@@ -11,14 +11,14 @@
       (string-join (map symbol->string fields) ",")
       " FROM "
       (symbol->string table))]
-    [(list (list fields ...) (list 'from table) (list 'where (list '= id val)))
-     (let [(whereeq (string-append (symbol->string id) " = " (number->string val)))]
-       (string-append
-        (string-join (map symbol->string fields) ",")
-        " FROM "
-        (symbol->string table)
-        " WHERE "
-        whereeq))]))
+    [(list (list fields ...) (list 'from table) (list 'where (list where-exprs ...)))
+     (let [(joined-fields (join-fields fields))
+           (whereeq (where->string where-exprs))]
+       (string-append (join-fields fields)
+                      " FROM "
+                      (symbol->string table)
+                      " WHERE "
+                      whereeq))]))
 
 (define-syntax-rule (insert exprs ...)
   (string-append
@@ -42,9 +42,26 @@
       
 
 (define (join-fields fields)
+  (string-join (map field->string fields) ","))
+
+(define (where->string . exprs)
+  (match exprs
+    [(list (list '= id val))
+     (string-append
+      (field->string id)
+      " = "
+      (field->string val))]
+    [(list (list 'and (list '= id val) (list '= id2 val2)))
+     (string-append
+      (field->string id)
+      " = "
+      (field->string val)
+      " AND "
+      (field->string id2)
+      " = "
+      (field->string val2))]))
+
   (define (field->string field)
     (if (number? field)
         (number->string field)
         (symbol->string field)))
-  
-  (string-join (map field->string fields) ","))
