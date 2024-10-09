0e542d51a84b2e3aa6df7d7eb51f17cc88565c14
Job Hernandez <jobhdezlara93@gmail.com>
2024-10-08 18:29:42 -0700

    add tests

---
+++ tests.rkt
@@ -0,0 +1,50 @@
+#lang racket
+(require test-engine/racket-tests)
+(require "sqly.rkt")
+
+(check-expect
+ (select (book author) (from books) (where (and (> id 3) (< id 6))))
+ '("SELECT book,author FROM books WHERE id > $1 AND id < $2" 3 6))
+
+(check-expect
+ (select (book author) (from books))
+ '("SELECT book,author FROM books"))
+
+(check-expect
+ (select (book author) (from books) (limit 10))
+ '("SELECT book,author FROM books LIMIT $1" 10))
+
+(check-expect
+ (select (book author) (from books) (order-by author ASC))
+ '("SELECT book,author FROM books ORDER BY $1 ASC" "author"))
+
+(check-expect
+ (select (book author) (from books) (where (= id 2)))
+ '("SELECT book,author FROM books WHERE id=$1" 2))
+
+(check-expect
+ (select (book author) (from books) (left-join books2 on (= books-id books2-id)))
+ '("SELECT book,author FROM books LEFT JOIN books2 ON $1  = $2" "books-id" "books2-id"))
+
+(check-expect
+ (select (book author) (from books) (where (books (in 3 4 5))))
+ '("SELECT book,author FROM books WHERE books in ($1,$2,$3)" 3 4 5))
+
+(check-expect
+ (insert into books (author book) (values (herman sicp)))
+ '("INSERT into books(author,book) values ($1,$2)" "herman" "sicp"))
+
+(check-expect
+ (update books (set ((= date 1) (= publisher 2))))
+ '("UPDATE books SET date = $1, publisher = $2" 1 2))
+
+(check-expect
+ (update books (set ((= date 1) (= f 3))) (where (= id 1)))
+ '("UPDATE books SET date = $1, f = $2 WHERE id=$3" 1 3 1))
+
+(check-expect
+ (update books (set ((= date 1) (= f 4))) (where (and (> id 3) (< id 5))))
+ '("UPDATE books SET date = $1, f = $2 WHERE id > $3 AND id < $4" 1 4 3 5))
+
+(test)
+ 
